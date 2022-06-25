2fec8255ebf160369395635a9629f80438713e7f
Aditya-Bani <bani96963@gmail.com>
2022-06-25 18:40:18 +0700

    commit baru 14

---
+++ assets/code/lab11_ci/ci4-prak14/app/Views/template/admin_footer.php
@@ -0,0 +1,8 @@
+                </section>
+            </section>
+            <footer>
+                <p>&copy; 2022 - Universitas Pelita Bangsa</p>
+            </footer>
+        </div>
+    </body>
+</html>
